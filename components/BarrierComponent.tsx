f08cc9b395ffd6642bdfa15e77c118084c96373a
Berke Duzgun <duzgunberke@gmail.com>
2024-03-21 02:29:54 +0300

    layout and base refactor

--- components/BarrierComponent.tsx
+++ components/BarrierComponent.tsx
@@ -1,10 +1,8 @@
-import { Button } from "./ui/button";
 
 export default function BarrierComponent() {
     return (
       <div >
         <div>Barier</div>
-        <Button size="sm" className="bg-red-700">Selam ben buton</Button>
       </div>
     );
   }
