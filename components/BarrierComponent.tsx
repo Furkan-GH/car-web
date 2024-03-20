36944787154c888cebbf3a093ca66b9025e53bb5
Furkan <furkan.ozkeroglu@hotmail.com>
2024-03-21 02:31:30 +0300

    barrier refactor.

--- components/BarrierComponent.tsx
+++ components/BarrierComponent.tsx
@@ -1,11 +1,23 @@
 import { Button } from "./ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 
 export default function BarrierComponent() {
-    return (
-      <div >
-        <div>Barier</div>
-        <Button size="sm" className="bg-red-700">Selam ben buton</Button>
+  return (
+    <div className="">
+      <div>Barier Control</div>
+      <div className="relative">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Slow</SelectItem>
+          <SelectItem value="dark">Mid</SelectItem>
+          <SelectItem value="system">Fast</SelectItem>
+        </SelectContent>
+      </Select>
       </div>
-    );
-  }
-  
+      <Button size="sm" className="border mt-4 relative">Open</Button>
+    </div>
+  );
+}
