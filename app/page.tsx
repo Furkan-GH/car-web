2985d1630e4d2fa56f2b94749de14b22fbb3a089
Furkan <furkan.ozkeroglu@hotmail.com>
2024-03-20 23:23:31 +0300

    component was added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import BarrierComponent from "@/components/BarrierComponent";
+import FanComponent from "@/components/FanComponent";
 import WaterTankComponent from "@/components/WaterTankComponent";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Anasayfa</div>
-      <BarrierComponent/>
-      <WaterTankComponent/>
+      <BarrierComponent />
+      <WaterTankComponent />
+      <FanComponent />
     </main>
   );
 }
---
+++ components/FanComponent.tsx
@@ -0,0 +1,8 @@
+export default function FanComponent() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>Fan</div>
+      </main>
+    );
+  }
+  
