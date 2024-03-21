566c40948b0052a3e59d6ce75a06e52fd26fba10
Berke Duzgun <duzgunberke@gmail.com>
2024-03-21 03:19:59 +0300

    add mock post api

--- app/api/mock/route.ts
+++ app/api/mock/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const message = {
-    "Deneme Yapin": "Hadi hadi iyisiniz",
-    "Deneme verisi1": "Deneme verisi_11",
-    "Deneme verisi2": "Deneme verisi_22",
-    "Deneme verisi3": "Deneme verisi_33",
+    "Deneme Yapin - GET": "Hadi hadi iyisiniz",
+    "Deneme verisi1 - GET": "Deneme verisi_11",
+    "Deneme verisi2 - GET": "Deneme verisi_22",
+    "Deneme verisi3 - GET": "Deneme verisi_33",
   };
     try {
       const messageBody = JSON.stringify(message);
@@ -20,3 +20,24 @@ export async function GET() {
       });
     }
 }
+
+export async function POST() {
+    const message = {
+      "Deneme Yapin - POST": "Hadi hadi iyisiniz",
+      "Deneme verisi1 - POST": "Deneme verisi_11",
+      "Deneme verisi2 - POST": "Deneme verisi_22",
+      "Deneme verisi3 - POST": "Deneme verisi_33",
+    };
+      try {
+        const messageBody = JSON.stringify(message);
+  
+        return new NextResponse(messageBody, {
+          status: 200
+        });
+      } catch (error) {
+        console.error(error); 
+        return new NextResponse(JSON.stringify({ error: "Internal Server Error" }), {
+          status: 500,
+        });
+      }
+  }
