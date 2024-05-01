e503f397d365bcc6aee26bbc4fbecc3a501640f3
Berke Duzgun <duzgunberke@gmail.com>
2024-05-01 16:34:35 +0300

    fix

--- actions/barrier-action.ts
+++ actions/barrier-action.ts
@@ -5,6 +5,7 @@ import { OperationStatus } from "@prisma/client";
 
 export const UpdateBarrierAction = async (id: string, barrierStatus: OperationStatus) => {
     try {
+      // (TODO) All sensor action file create and write this code 
       console.log("ID = "+id+"Barier Status = "+barrierStatus);
       const updatedCar = await db.washedCar.update({
         where: { id },
