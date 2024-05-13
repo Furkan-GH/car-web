82cb9dd915edfda65e3c22f2acaa95aebba87d84
Furkan <furkan.ozkeroglu@hotmail.com>
2024-05-13 23:03:56 +0300

    cam.try

--- components/CameraComponent.tsx
+++ components/CameraComponent.tsx
@@ -7,7 +7,7 @@ export default function CameraComponent() {
       {/* <div className='flex justify-center min-h-[600px]'>
         <Video className='max-w-screen-lg justify-center' src={awesomeVideo} placeholder="blur" />
       </div> */}
-      <iframe src="/" width="100%" height="300">
+      <iframe src="https://carweb31.vercel.app/" width="100%" height="300">
       </iframe>
 
     </>
