da7c0cddd26a1819a63eb9cf35dace956e7f2a69
Furkan <furkan.ozkeroglu@hotmail.com>
2024-05-13 22:59:45 +0300

    try-cam

--- components/CameraComponent.tsx
+++ components/CameraComponent.tsx
@@ -2,14 +2,19 @@ import Video from 'next-video';
 const awesomeVideo = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
 export default function CameraComponent() {
-    return (
-      <>
-        <div className='flex justify-center min-h-[600px]'>
-          <Video className='max-w-screen-lg justify-center' src={awesomeVideo} placeholder="blur" />
-        </div>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      {/* <div className='flex justify-center min-h-[600px]'>
+        <Video className='max-w-screen-lg justify-center' src={awesomeVideo} placeholder="blur" />
+      </div> */}
+      <iframe src="/default.asp" width="100%" height="300">
+      </iframe>
 
-  // https://www.npmjs.com/package/next-video
+      <iframe src="/default.asp" width="100%" height="300">
+      </iframe>
+    </>
+  );
+}
+
+
+// https://www.npmjs.com/package/next-video
